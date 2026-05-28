3792de50551766e9adc936591fa09b3d43ca9227
Don Pham <donphamnguyen@gmail.com>
2026-05-27 20:16:59 -0700

    Added more commands

--- commands.bash
+++ commands.bash
@@ -9,6 +9,11 @@ python embed_exercises.py --input-path "processed_sequences/all_exercises.jsonl"
 # Recommender Evaluation
 python recommend_eval.py --model embedding_similarity --k-values 5,10
 
+# Baseline Popularity Model
+python recommend_eval.py --model popularity --k-values 5,10
+
+# Baseline Random SASRec Model
+python train_sasrec.py --epochs 5 --batch-size 64 --max-len 30 --k-values 5,10 --save-dir sasrec_random
 
 # Train and evaluate SASRec
-python train_sasrec.py --epochs 5 --batch-size 64 --max-len 30 --use-pretrained-embeddings --use-outcome-embedding --k-values 5,10 --save-dir sasrec_semantic_outcome
+python train_sasrec.py --epochs 5 --batch-size 128 --max-len 50 --use-pretrained-embeddings --use-outcome-embedding --k-values 5,10 --save-dir sasrec_semantic_outcome
